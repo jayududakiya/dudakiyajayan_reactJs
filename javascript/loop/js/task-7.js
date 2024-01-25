02667d451673337a8cecec0df868819097cd51ef
jayan <jayududakiya@gmail.com>
2024-01-25 18:46:46 +0530

    25/01/2024

--- javascript/loop/js/task-7.js
+++ javascript/loop/js/task-7.js
@@ -1,5 +1,37 @@
 {
+    // Check if a word is a palindrome using a while loop 
 
-    
+    let i , n =33;
+
+    if(n<10)
+    {
+        for(i=0;i<10;i++)
+        {
+            if(i!=n)
+            {
+                // document.write("your number is not between 1 to 9 mech " + n + "<br>");
+                continue;
+            }
+            else
+            {
+                document.write(i + "<br>");
+            }
+        }
+    }
+    else
+    {
+        for(i=10;i<100;i++)
+        {
+            if(i-i==0 && i == n)
+            {
+                document.write("this is i " +i + "<br>");
+                // if(i===n)
+                // {
+                //     document.write(" this " + i + "<br>");
+                //     break; 
+                // }
+            }
+        }
+    }
 
 }
