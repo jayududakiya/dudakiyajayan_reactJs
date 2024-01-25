72fa71c375cc91a0497c12e23c116b8da7d204fe
jayan <jayududakiya@gmail.com>
2024-01-25 19:44:15 +0530

    25/01/2024

--- javascript/loop/js/task-7.js
+++ javascript/loop/js/task-7.js
@@ -1,37 +1,38 @@
 {
     // Check if a word is a palindrome using a while loop 
+    let a, b = 10 , c = 0 , i = 0 ;
 
-    let i , n =33;
 
-    if(n<10)
-    {
-        for(i=0;i<10;i++)
-        {
-            if(i!=n)
-            {
-                // document.write("your number is not between 1 to 9 mech " + n + "<br>");
-                continue;
-            }
-            else
-            {
-                document.write(i + "<br>");
-            }
-        }
-    }
-    else
-    {
-        for(i=10;i<100;i++)
-        {
-            if(i-i==0 && i == n)
-            {
-                document.write("this is i " +i + "<br>");
-                // if(i===n)
-                // {
-                //     document.write(" this " + i + "<br>");
-                //     break; 
-                // }
-            }
-        }
-    }
+    // for(i = 0 ; i <= 40; i++ )
+    // {
+    //     i = b;
+    //     b = c ;
+    //     c = i + b ;
+    //     document.write("this is C : " + c + "<br>");
+    //     // document.write("this is B : " + b + "<br>");
+    //     // document.write("this is I : " + i + "<br>");
+    // }
+//    for(a=1;a<=20;a++)
+//    {
+//         a = b;
+//         b = c;
+//         c= a+b;
+//         ++i;
+//         console.log(c+i);
+//     }
+//     console.log( "this is a " +a);
+//     console.log( "this is b " +b);
 
+
+        // c = 0 + a + i++;
+        // for(i=0;i<=10;i++)
+        // {
+        //     c = c+ b + 1;
+        //     console.log(c);
+        // if(c>100)
+        // {
+        //     c = c + b + 1;
+        //     console.log(c+1);
+        // }
+        // }
 }
