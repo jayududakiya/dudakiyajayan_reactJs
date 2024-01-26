5dc484eb2fed878ad945b919e3be2cf54c6a4e87
jayan <jayududakiya@gmail.com>
2024-01-26 17:15:54 +0530

    26/01/2024

--- javascript/loop/js/task-7.js
+++ javascript/loop/js/task-7.js
@@ -1,38 +1,17 @@
 {
     // Check if a word is a palindrome using a while loop 
-    let a, b = 10 , c = 0 , i = 0 ;
-
-
-    // for(i = 0 ; i <= 40; i++ )
-    // {
-    //     i = b;
-    //     b = c ;
-    //     c = i + b ;
-    //     document.write("this is C : " + c + "<br>");
-    //     // document.write("this is B : " + b + "<br>");
-    //     // document.write("this is I : " + i + "<br>");
-    // }
-//    for(a=1;a<=20;a++)
-//    {
-//         a = b;
-//         b = c;
-//         c= a+b;
-//         ++i;
-//         console.log(c+i);
-//     }
-//     console.log( "this is a " +a);
-//     console.log( "this is b " +b);
-
+    let  b = 10 , c = 0 , i;
 
         // c = 0 + a + i++;
-        // for(i=0;i<=10;i++)
-        // {
-        //     c = c+ b + 1;
-        //     console.log(c);
-        // if(c>100)
-        // {
-        //     c = c + b + 1;
-        //     console.log(c+1);
-        // }
-        // }
+        for(i=0;i<=10;i++)
+        {
+            c = c+ b + 1;
+            document.write(c + " ");
+            if(c>99)
+            {
+                b = 100;
+                c = c + b + 10 + i;
+                document.write(" " + c + " " );
+            }
+        }
 }
