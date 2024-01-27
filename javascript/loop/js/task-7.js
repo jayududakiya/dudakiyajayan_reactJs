2fdece18b19f1509248ed6d5aec1384cfec80185
jayan <jayududakiya@gmail.com>
2024-01-27 16:56:33 +0530

    27/01/2024

--- javascript/loop/js/task-7.js
+++ javascript/loop/js/task-7.js
@@ -3,15 +3,28 @@
     let  b = 10 , c = 0 , i;
 
         // c = 0 + a + i++;
-        for(i=0;i<=10;i++)
-        {
-            c = c+ b + 1;
-            document.write(c + " ");
-            if(c>99)
-            {
-                b = 100;
-                c = c + b + 10 + i;
-                document.write(" " + c + " " );
-            }
+        // for(i=0;i<=10;i++)
+        // {
+        //     c = c+ b + 1;
+        //     document.write(c + " ");
+        //     if(c>99)
+        //     {
+        //         b = 100;
+        //         c = c + b + 10 + i;
+        //         document.write(" " + c + " " );
+        //     }
+        // }
+
+
+        let y= 0 ;
+        var  x=124;
+
+        while(x!=0){
+            y=y*10+x%10;
+            // x/=10;
+            // x = Math.floor(x/10);
+            x = x/10;
         }
+        console.log(x);
+        console.log(y);
 }
