a531792d163634cec1dbc607e03fcf201758baa5
jayan <jayududakiya@gmail.com>
2024-01-31 08:53:52 +0530

    31/01/2024

--- javascript/Js/tempCodeRunnerFile.js
+++ javascript/Js/tempCodeRunnerFile.js
@@ -1,9 +1,21 @@
-let a = 30 , b = 20 , c ;
+// let a = 30 , b = 20 , c ;
 
-c = a ; // c = 30 and a is empty now 
-a = b ; // a = 20 and b is empty now 
-b = c; //  b = c(30) and c is 30 now 
+// c = a ; // c = 30 and a is empty now 
+// a = b ; // a = 20 and b is empty now 
+// b = c; //  b = c(30) and c is 30 now 
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// let n1 =  Number(true);
+//    let n2 =  Number(false);
+//    let n3 =  Number("10");	
+//    let n4 =  Number("  10");	
+//    let n5=  Number("10  ");
+//    let n6 =  Number(" 10  ");
+//    let n7 =  Number("10.33");	
+//    let n8 =  "10,33";	
+//    let n9 =  Number("10 33");	
+//    let n10 =  Number("John");
--- javascript/loop/js/task-11.js
+++ javascript/loop/js/task-11.js
@@ -4,15 +4,17 @@
 {
     let i = prompt("Find the first odd number in a sequence using a while loop and break statement \n Enter a starting number value : " ) , num = prompt("enter a ending number value : " );  
 
+    //let i = 5 , num=10;
+
+
     while(i<=num)
     {
-        i++;
-        if(1 == i%2)
+        if(i%2 !== 0)
         {
             document.write(" the first odd number is " + i );
             break ;
         }
-        // document.write(i);
+        i++;
     }
 
 }
